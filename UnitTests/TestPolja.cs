84569984387fe12b3c1fcddd1ade376f58aed436
julijan <julijan.sribar@gmail.com>
2016-03-14 20:56:51 +0100

    Zadavanje projekta, početna definicija klase POlje i jedinični test

---
+++ UnitTests/TestPolja.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PotapanjeBrodova;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestPolja
+    {
+        [TestMethod]
+        public void Polje_SvojstvaRedakStupacJednakaSuVrijednostimaZadanimKonstruktorom()
+        {
+            Polje p = new Polje(1, 2);
+            Assert.AreEqual(1, p.Redak);
+            Assert.AreEqual(2, p.Stupac);
+        }
+    }
+}
