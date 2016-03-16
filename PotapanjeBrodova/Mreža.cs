a3291afc6ad6ff9c973e57e8902cf87718416ccc
julijan <julijan.sribar@gmail.com>
2016-03-16 21:17:08 +0100

    Dodaj klasu Mreža

---
+++ PotapanjeBrodova/Mreža.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Mreža
+    {
+        public Mreža(int redaka, int stupaca)
+        {
+            this.redaka = redaka;
+            this.stupaca = stupaca;
+            polja = new Polje[redaka, stupaca];
+            for (int r = 0; r < redaka; ++r)
+            {
+                for (int s = 0; s < stupaca; ++s)
+                    polja[r, s] = new Polje(r, s);
+            }
+        }
+
+        public List<Polje> DajSlobodnaPolja()
+        {
+            List<Polje> slobodnaPolja = new List<Polje>();
+            for (int r = 0; r < redaka; ++r)
+            {
+                for (int s = 0; s < stupaca; ++s)
+                {
+                    if (polja[r, s] != null)
+                        slobodnaPolja.Add(polja[r, s]);
+                }
+            }
+            return slobodnaPolja;
+        }
+
+        public void EliminirajPolje(int redak, int stupac)
+        {
+            polja[redak, stupac] = null;
+        }
+
+        private Polje[,] polja;
+        private int redaka;
+        private int stupaca;
+    }
+}
---
+++ UnitTests/TestMreže.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Summary description for TestMreže
+    /// </summary>
+    [TestClass]
+    public class TestMreže
+    {
+        [TestMethod]
+        public void Mreža_DajSlobodnaPoljaInicijalnoDajeSvaPoljaUMreži()
+        {
+            Mreža m = new Mreža(10, 10);
+            Assert.AreEqual(100, m.DajSlobodnaPolja().Count);
+            Assert.IsTrue(m.DajSlobodnaPolja().Exists(polje => polje.Redak == 1 && polje.Stupac == 1));
+        }
+
+        [TestMethod]
+        public void Mreža_DajSlobodnaPoljaNakonEliminiranjaJednogPoljaVraćaOstatak()
+        {
+            Mreža m = new Mreža(10, 10);
+            m.EliminirajPolje(1, 1);
+            Assert.AreEqual(99, m.DajSlobodnaPolja().Count);
+            Assert.IsFalse(m.DajSlobodnaPolja().Exists(polje => polje.Redak == 1 && polje.Stupac == 1));
+        }
+
+        [TestMethod]
+        public void Mreža_DajSlobodnaPoljaNakonEliminiranjaDvaPoljaVraćaOstatak()
+        {
+            Mreža m = new Mreža(10, 10);
+            m.EliminirajPolje(1, 1);
+            m.EliminirajPolje(2, 2);
+            Assert.AreEqual(98, m.DajSlobodnaPolja().Count);
+            Assert.IsFalse(m.DajSlobodnaPolja().Exists(polje => polje.Redak == 1 && polje.Stupac == 1));
+            Assert.IsFalse(m.DajSlobodnaPolja().Exists(polje => polje.Redak == 2 && polje.Stupac == 2));
+        }
+
+        [TestMethod]
+        public void Mreža_DajSlobodnaPoljaNakonEliminiranjaDvaIstaPoljaVraćaOstatak()
+        {
+            Mreža m = new Mreža(10, 10);
+            m.EliminirajPolje(1, 1);
+            m.EliminirajPolje(1, 1);
+            Assert.AreEqual(99, m.DajSlobodnaPolja().Count);
+        }
+
+    }
+}
