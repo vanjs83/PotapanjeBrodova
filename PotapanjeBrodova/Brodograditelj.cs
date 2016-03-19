2c347f6e5d768d6d25e6680e3c8fba2628120297
julijan <julijan.sribar@gmail.com>
2016-03-19 21:10:58 +0100

    Dodaj definiciju klase Brodograditelj
    - Početna definicija klase Brodograditelj koja je zadužena za slaganje
      flote.
    - Deklaracija klase Flota, dodana zbog definicije metode u klasi
      Brodograditelj.

---
+++ PotapanjeBrodova/Brodograditelj.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Brodograditelj
+    {
+        public Brodograditelj(int redak, int stupaca, int[] duljineBrodova)
+        {
+        }
+
+        public Flota SložiFlotu()
+        {
+            Flota f = new Flota();
+            // napravi mrežu
+
+            // za svaku duljinu broda:
+
+                // od mreže zatraži slobodna polja
+
+                // izaberi početno polje za brod
+
+                // napravi brod i dodaj ga u flotu
+
+                // mreži kaži da eliminra polja od i oko broda
+
+            return f;
+        }
+
+
+    }
+}
