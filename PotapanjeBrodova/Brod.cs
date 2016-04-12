438596c34993591272c5f721ede7e414bc11b44b
julijan <julijan.sribar@gmail.com>
2016-04-12 20:23:06 +0200

    Dodaj definiciju klase Brod

---
+++ PotapanjeBrodova/Brod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Brod
+    {
+        public Brod(IEnumerable<Polje> polja)
+        {
+            this.polja = polja;
+        }
+
+        public readonly IEnumerable<Polje> polja;
+    }
+}
