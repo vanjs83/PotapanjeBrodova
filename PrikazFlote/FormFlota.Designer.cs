fee54bc89e5e71090bd69c94999aa64e642b0c0a
jsribar <julijan.sribar@gmail.com>
2016-04-12 23:29:24 +0200

    Promijeni ponašanje tipke na formi da se pomiče s promjenom veličine forme

--- PrikazFlote/FormFlota.Designer.cs
+++ PrikazFlote/FormFlota.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.mrežaZaFlotu = new PrikazFlote.MrežaZaFlotu();
             this.buttonSložiFlotu = new System.Windows.Forms.Button();
+            this.mrežaZaFlotu = new PrikazFlote.MrežaZaFlotu();
             ((System.ComponentModel.ISupportInitialize)(this.mrežaZaFlotu)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonSložiFlotu
+            // 
+            this.buttonSložiFlotu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSložiFlotu.Location = new System.Drawing.Point(364, 436);
+            this.buttonSložiFlotu.Name = "buttonSložiFlotu";
+            this.buttonSložiFlotu.Size = new System.Drawing.Size(75, 23);
+            this.buttonSložiFlotu.TabIndex = 1;
+            this.buttonSložiFlotu.Text = "Složi";
+            this.buttonSložiFlotu.UseVisualStyleBackColor = true;
+            this.buttonSložiFlotu.Click += new System.EventHandler(this.buttonSložiFlotu_Click);
             // 
             // mrežaZaFlotu
             // 
@@ -44,16 +55,6 @@
             this.mrežaZaFlotu.Size = new System.Drawing.Size(427, 404);
             this.mrežaZaFlotu.TabIndex = 0;
             this.mrežaZaFlotu.TabStop = false;
-            // 
-            // buttonSložiFlotu
-            // 
-            this.buttonSložiFlotu.Location = new System.Drawing.Point(364, 436);
-            this.buttonSložiFlotu.Name = "buttonSložiFlotu";
-            this.buttonSložiFlotu.Size = new System.Drawing.Size(75, 23);
-            this.buttonSložiFlotu.TabIndex = 1;
-            this.buttonSložiFlotu.Text = "Složi";
-            this.buttonSložiFlotu.UseVisualStyleBackColor = true;
-            this.buttonSložiFlotu.Click += new System.EventHandler(this.buttonSložiFlotu_Click);
             // 
             // FormFlota
             // 
