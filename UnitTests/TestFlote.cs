b2ad16cc15d79c286efd66adca1e26b7912eef28
jsribar <julijan.sribar@gmail.com>
2016-04-12 22:14:46 +0200

    Dodaj test za Flotu

---
+++ UnitTests/TestFlote.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PotapanjeBrodova;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestFlote
+    {
+        private IEnumerable<Polje> SložiPolja(Smjer smjer, Polje početno, int duljinaBroda)
+        {
+            Brodograditelj b = new Brodograditelj();
+            return b.DajPoljaZaBrod(smjer, početno, duljinaBroda);
+        }
+
+        [TestMethod]
+        public void Flota_DodajBrodaTriRazličitaBrodaSlažeFlotuOdTriBroda()
+        {
+            Flota f = new Flota();
+            Brod b1 = new Brod(SložiPolja(Smjer.Horizontalno, new Polje(0, 0), 5));
+            f.DodajBrod(b1);
+            Brod b2 = new Brod(SložiPolja(Smjer.Vertikalno, new Polje(1, 3), 4));
+            f.DodajBrod(b2);
+            Brod b3 = new Brod(SložiPolja(Smjer.Horizontalno, new Polje(4, 5), 3));
+            f.DodajBrod(b3);
+
+            Assert.AreEqual(3, f.Brodovi.Count());
+            Assert.IsTrue(f.Brodovi.Contains(b1));
+            Assert.IsTrue(f.Brodovi.Contains(b2));
+            Assert.IsTrue(f.Brodovi.Contains(b3));
+        }
+    }
+}
