b488936f10b992ba5346bfb45266ad6f6b6028cb
jsribar <julijan.sribar@gmail.com>
2016-04-12 22:26:35 +0200

    Dodaj test za metodu SložiFlotu

--- UnitTests/TestBrodograditelja.cs
+++ UnitTests/TestBrodograditelja.cs
@@ -256,5 +256,21 @@ namespace UnitTests
             Assert.IsTrue(zaEliminirati.Contains(new Polje(početnoPolje.Redak - 1, 1)));
             Assert.IsTrue(zaEliminirati.Contains(new Polje(redaka - 1, 1)));
         }
+
+        [TestMethod]
+        public void Brodograditelj_SložiFlotuVraćaFlotuSaZadanimBrojemBrodova()
+        {
+            int redaka = 10;
+            int stupaca = 10;
+            int[] duljineBrodova = new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 };
+            Brodograditelj b = new Brodograditelj();
+            var flota = b.SložiFlotu(redaka, stupaca, duljineBrodova);
+            Assert.AreEqual(10, flota.BrojBrodova);
+            // provjerimo koliko je brodova zadane duljine
+            Assert.AreEqual(1, flota.Brodovi.Count(brod => brod.Duljina == 5));
+            Assert.AreEqual(2, flota.Brodovi.Count(brod => brod.Duljina == 4));
+            Assert.AreEqual(3, flota.Brodovi.Count(brod => brod.Duljina == 3));
+            Assert.AreEqual(4, flota.Brodovi.Count(brod => brod.Duljina == 2));
+        }
     }
 }
