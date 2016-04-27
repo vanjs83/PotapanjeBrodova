904d98bda9b28651c3367ffd3201609a07b272d9
julijan <julijan.sribar@gmail.com>
2016-04-27 21:14:20 +0200

    Dodaj klasu Topništvo Dodaj test za topništvo

---
+++ PotapanjeBrodova/Topništvo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public enum TaktikaGađanja
+    {
+        Napipavanje,
+        Okruživanje,
+        SustavnoUništavanje
+    }
+
+    public class Topništvo
+    {
+        public Topništvo()
+        {
+            PromijeniTaktikuUNapipavanje();
+        }
+
+        public Polje UputiPucanj()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ObradiGađanje(RezultatGađanja rezultat)
+        {
+
+        }
+
+        private void PromijeniTaktikuUNapipavanje()
+        {
+            TrenutnaTaktika = TaktikaGađanja.Napipavanje;
+        }
+
+        private void PromijeniTaktikuUOkruživanje()
+        {
+            TrenutnaTaktika = TaktikaGađanja.Okruživanje;
+        }
+
+        private void PromijeniTaktikuUSustavnoUništavanje()
+        {
+            TrenutnaTaktika = TaktikaGađanja.SustavnoUništavanje;
+        }
+
+        public TaktikaGađanja TrenutnaTaktika
+        {
+            get; private set;
+        }
+    }
+}
---
+++ UnitTests/TestTopništva.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestTopništva
+    {
+        [TestMethod]
+        public void Topništvo_PočetnaTaktikaGađanjaJeNapipavanje()
+        {
+            Topništvo t = new Topništvo();
+            Assert.AreEqual(TaktikaGađanja.Napipavanje, t.TrenutnaTaktika);
+        }
+    }
+}
