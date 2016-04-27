0b91b0af551a3f932264df555873f383ed795178
julijan <julijan.sribar@gmail.com>
2016-04-27 21:27:04 +0200

    Dodaj testove za promjenu taktiku

--- UnitTests/TestTopništva.cs
+++ UnitTests/TestTopništva.cs
@@ -13,5 +13,47 @@ namespace UnitTests
             Topništvo t = new Topništvo();
             Assert.AreEqual(TaktikaGađanja.Napipavanje, t.TrenutnaTaktika);
         }
+
+        [TestMethod]
+        public void Topništvo_TaktikaGađanjaNakonPrvogPogotkaJeOkruživanje()
+        {
+            Topništvo t = new Topništvo();
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            Assert.AreEqual(TaktikaGađanja.Okruživanje, t.TrenutnaTaktika);
+        }
+
+        [TestMethod]
+        public void Topništvo_TaktikaGađanjaNakonDrugogPogotkaJeSustavnoUništavanje()
+        {
+            Topništvo t = new Topništvo();
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            Assert.AreEqual(TaktikaGađanja.SustavnoUništavanje, t.TrenutnaTaktika);
+        }
+
+        [TestMethod]
+        public void Topništvo_TaktikaGađanjaNakonPotonućaJeNapipavanje()
+        {
+            Topništvo t = new Topništvo();
+            t.ObradiGađanje(RezultatGađanja.Potonuće);
+            Assert.AreEqual(TaktikaGađanja.Napipavanje, t.TrenutnaTaktika);
+        }
+
+        [TestMethod]
+        public void Topništvo_TaktikaGađanjaNakonPromašajaSeNeMijenja()
+        {
+            Topništvo t = new Topništvo();
+            // inicijalno je Napipavanje
+            t.ObradiGađanje(RezultatGađanja.Promašaj);
+            Assert.AreEqual(TaktikaGađanja.Napipavanje, t.TrenutnaTaktika);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            // nakon prvog pogotka je Okruživanje
+            t.ObradiGađanje(RezultatGađanja.Promašaj);
+            Assert.AreEqual(TaktikaGađanja.Okruživanje, t.TrenutnaTaktika);
+            t.ObradiGađanje(RezultatGađanja.Pogodak);
+            // nakon drugog pogtka je SustavnoUništavanje
+            t.ObradiGađanje(RezultatGađanja.Promašaj);
+            Assert.AreEqual(TaktikaGađanja.SustavnoUništavanje, t.TrenutnaTaktika);
+        }
     }
 }
