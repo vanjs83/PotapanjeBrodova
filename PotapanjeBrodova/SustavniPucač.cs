9340c47e5a41b0d8afcfc5fd8e611fc8670e86de
vanjs83 <tihomir.vanjurek@gmail.com>
2016-05-11 20:57:07 +0200

    Add class SustavniPucač

---
+++ PotapanjeBrodova/SustavniPucač.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    class SustavniPucač : IPucač
+    {
+        public Polje UputiPucanj()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
