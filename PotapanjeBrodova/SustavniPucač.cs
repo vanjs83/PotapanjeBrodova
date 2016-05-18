7cce56666b4cd0445291b53642d10d74cc8711f9
vanjs83 <tihomir.vanjurek@gmail.com>
2016-05-18 20:46:37 +0200

    implementiraj sustavnog pucača

--- PotapanjeBrodova/SustavniPucač.cs
+++ PotapanjeBrodova/SustavniPucač.cs
@@ -9,13 +9,60 @@ namespace PotapanjeBrodova
     {  public SustavniPucač(IEnumerable<Polje> pogođena, Mreža mreža)
         {
             pogođenaPolja = new List<Polje>(pogođena);
+            pogođenaPolja.Sort((a, b ) => a.Redak - b.Redak + a.Stupac - b.Stupac);
             this.mreža = mreža;
         }
         public Polje UputiPucanj()
         {
-            throw new NotImplementedException();
+            Orijentacija o = DajOrijentaciju();
+            var liste = DajPoljaUNastavku(o);
+            if (liste.Count() == 1)
+             return   liste.First().First();
+            int indeks = slučajni.Next(liste.Count());
+            return liste.ElementAt(indeks).First();
+
+  
+        
         }
+        private Orijentacija DajOrijentaciju()
+        {
+            if (pogođenaPolja[0].Redak == pogođenaPolja[1].Redak)
+                return Orijentacija.Horizontalno;
+            return Orijentacija.Vertikalno;
+        }
+        private IEnumerable<IEnumerable<Polje>> DajPoljaUNastavku(Orijentacija orijenatcija)
+        {
+            switch (orijenatcija)
+            {
+                case Orijentacija.Vertikalno:
+                    return DajPoljaUNastavku(Smjer.Gore, Smjer.Dolje);
+                case Orijentacija.Horizontalno:
+                    return DajPoljaUNastavku(Smjer.Lijevo, Smjer.Desno);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+        private IEnumerable<IEnumerable<Polje>> DajPoljaUNastavku(Smjer smjer1, Smjer smjer2)
+            
+        {
+            List<IEnumerable<Polje>> liste = new List<IEnumerable<Polje>>();
+            int redak0 = pogođenaPolja[0].Redak;
+            int stupac0 = pogođenaPolja[0].Stupac;
+            var l1=mreža.DajPoljaUZadanomSmjeru(redak0, stupac0, smjer1);
+            if (l1.Count() > 0)
+                liste.Add(l1);
+            int n = pogođenaPolja.Count() - 1;
+            int redakN = pogođenaPolja[n].Redak;
+            int stupacN = pogođenaPolja[n].Stupac;
+            var l2 = mreža.DajPoljaUZadanomSmjeru(redakN, stupacN, smjer2);
+            if (l2.Count() > 0)
+                liste.Add(l2);
+            return liste;
+
+        }
+
         List<Polje> pogođenaPolja;
         Mreža mreža;
+        Random slučajni = new Random();
     }
 }
---
+++ UnitTests/TestSustavnogPucača.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestSustavnogPucača
+    {
+        [TestMethod]
+        public void SustavniPucač_UputiPucanjVraćaLjevoIliDesnopoljeZaHorizontalniSmjer()
+        {
+            List<Polje> pogođenaPolja = new List<Polje> { new Polje(3, 2), new Polje(3, 3) };
+            Mreža mreža = new Mreža(10, 10);
+            SustavniPucač p = new SustavniPucač(pogođenaPolja, mreža);
+            List<Polje> kandidati = new List<Polje> { new Polje(3, 1), new Polje(3, 4) };
+            Assert.IsTrue(kandidati.Contains(p.UputiPucanj()));
+        }
+
+        [TestMethod]
+        public void SustavniPucač_UputiPucanjVraćaGoreIliDoljepoljeZaVertikalniSmjer()
+        {
+            List<Polje> pogođenaPolja = new List<Polje> { new Polje(2, 2), new Polje(3, 2) };
+            Mreža mreža = new Mreža(10, 10);
+            SustavniPucač p = new SustavniPucač(pogođenaPolja, mreža);
+            List<Polje> kandidati = new List<Polje> { new Polje(1, 2), new Polje(4, 2) };
+            Assert.IsTrue(kandidati.Contains(p.UputiPucanj()));
+        }
+
+        [TestMethod]
+        public void SustavniPucač_UputiPucanjVraćaDonjePoljeZaVertikalniSmjerUzRubMreže()
+        {
+            List<Polje> pogođenaPolja = new List<Polje> { new Polje(0, 2), new Polje(1, 2) };
+            Mreža mreža = new Mreža(10, 10);
+            SustavniPucač p = new SustavniPucač(pogođenaPolja, mreža);
+            Assert.AreEqual(new Polje(2, 2),p.UputiPucanj());
+        }
+
+        [TestMethod]
+        public void SustavniPucač_UputiPucanjVraćaDesnoPoljeZaHorizontalniSmjerAkoJeLjevoPoljeVećEliminirano()
+        {
+            List<Polje> pogođenaPolja = new List<Polje> { new Polje(3, 2), new Polje(3, 3) };
+            Mreža mreža = new Mreža(10, 10);
+            mreža.EliminirajPolje(3, 1);
+            SustavniPucač p = new SustavniPucač(pogođenaPolja, mreža);
+            List<Polje> kandidati = new List<Polje> { new Polje(3, 4) };
+            Assert.IsTrue(kandidati.Contains(p.UputiPucanj()));
+        }
+    }
+}
